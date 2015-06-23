d094d25f86f1a46672fd3a5e7cf6650448045d2f
Yu Jiang <yjiang26@gmail.com>
2015-06-22 22:48:42 -0400

    fix psi

--- adpSysWrapper.m
+++ adpSysWrapper.m
@@ -8,10 +8,10 @@ dx = susp_sys(x,u);       % dx as the first 1-4 states of the wrapper
 
 % Augmented part
 
-psi = Phi(x)';
+psi = Psi(x);
 
-PsiPsi = kron(psi,psi);   % Export \psi\otimes\psi as the 4+[1,20^2] states
-Psiu   = psi*u;           % Export \psi*u as the 4+[1,20^2]+ [1,20] states
+PsiPsi = kron(psi,psi);   % Export \psi\otimes\psi as the 4+[1,24^2] states
+Psiu   = psi*u;           % Export \psi*u as the 4+[1,24^2]+ [1,24] states
 Q = x'*x;                 % Export q(x) as the last state
 
 dX = [dx; 
--- main.m
+++ main.m
@@ -1,9 +1,11 @@
+% Main Script
 
 PhiPhi = [];
 PhiU = [];
+CostQ = [];
 
-X = zeros(1,425);
+X = zeros(1,4+24^2+24+1);
 [t,X] = ode45(@adpSysWrapper, [0,1], X(end,:));
-PhiPhi = [PhiPhi;
-	      ]
-PhiU = [PhiU; 
+PhiPhi = [PhiPhi;X(end,4 + (1:24^2))];
+PhiU = [PhiU; X(end,4 + 24^2 + (1:24))];
+CostQ = [CostQ; X(end,end)];
