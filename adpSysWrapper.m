6fb9be7387c1249f8a953749bc1917963329877c
yujiang <yujiang@AH-YUJIANG.dhcp.mathworks.com>
2015-06-23 10:08:13 -0400

    testing

--- adpSysWrapper.m
+++ adpSysWrapper.m
@@ -2,13 +2,13 @@ function dX = adpSysWrapper(t,X)
  
 % System dynamics part
 x = X(1:4);
-u = sin(t);
+u = -x(1)+sum(0.01*sin([1 3 7 11 13 15]*t));
 
 dx = susp_sys(x,u);       % dx as the first 1-4 states of the wrapper
 
 % Augmented part
 
-psi = Psi(x);
+psi = Psi_fun(x);
 
 PsiPsi = kron(psi,psi);   % Export \psi\otimes\psi as the 4+[1,24^2] states
 Psiu   = psi*u;           % Export \psi*u as the 4+[1,24^2]+ [1,24] states
--- main.m
+++ main.m
@@ -1,11 +1,50 @@
 % Main Script
 
-PhiPhi = [];
-PhiU = [];
+PsiPsi = [];
+PsiU = [];
+Phi = [];
 CostQ = [];
 
-X = zeros(1,4+24^2+24+1);
-[t,X] = ode45(@adpSysWrapper, [0,1], X(end,:));
-PhiPhi = [PhiPhi;X(end,4 + (1:24^2))];
-PhiU = [PhiU; X(end,4 + 24^2 + (1:24))];
-CostQ = [CostQ; X(end,end)];
+PhiLength = numel(Phi_fun(zeros(1,4)));
+PsiLength = numel(Psi_fun(zeros(1,4)));
+
+r = 1;  % Weight on u
+
+N = 100;
+IterMax = 10;  % Number of iterations
+
+X = [1,.3,1,-.2,zeros(1,PsiLength^2+PsiLength+1)];
+
+T = 0.01; % Length for each time interval
+
+% Data Collection
+for i = 1:N	
+	[t,X] = ode45(@adpSysWrapper, ...
+		          [i-1,i]*T, ...
+				  [X(end,1:4) zeros(1, PsiLength^2 + PsiLength + 1)]);
+	Phi    = [Phi; 
+		      Phi_fun(X(end,1:4))-Phi_fun(X(1,1:4))];        %#ok<AGROW>
+	PsiPsi = [PsiPsi;
+		      X(end,4 + (1:PsiLength^2))];                   %#ok<AGROW>
+	PsiU   = [PsiU; 
+		      X(end,4 + PsiLength^2 + (1:PsiLength))];       %#ok<AGROW>
+	CostQ = [CostQ; 
+		      X(end,end)];			                         %#ok<AGROW>
+end
+
+% Online Off-Policy Learning. Solve the matrix Ax = B 
+
+w = zeros(PsiLength,1);
+w(1) = 1;
+
+for i = 1:1%IterMax
+	A = [Phi -2*r*PsiU+2*r*PsiPsi*kron(w,ones(PsiLength))];
+    rank(A)
+	B = -(CostQ + PsiPsi*kron(w,w));
+	pw = A\B;
+  	p = pw(1:PhiLength)'
+	w = pw(PhiLength+1:end);
+end
+
+
+
